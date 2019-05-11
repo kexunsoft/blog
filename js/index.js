18311f63cdb8dc4c64601d83e73f722e03d51ce1
398LD <1813723783@qq.com>
2019-05-11 21:53:09 +0800

    更新功能

--- js/index.js
+++ js/index.js
@@ -3,7 +3,10 @@ $(function() {
 	$.get("config/blogConfig.json", function(data) {
 
 		$.each(data, function(index, item) {
-			console.log(item)
+			$(".list").append('<div class="item"> ' +
+				'<h3><a  href="' + item.href + '"  target="_blank">' + item.title + '</a><small>&nbsp;更新时间:' + item.date + '</small></h3>' +
+				'<p>' + item.desc + '</p>' +
+				'</div>')
 		});
 
 	})
